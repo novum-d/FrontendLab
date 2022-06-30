cffc62650c47ecdadd517a12e1b10d8c331807ff
hamadatomoki <hamada.tomoki01@gmail.com>
2022-06-30 17:45:49 +0900

    :books: Learn window list.

--- react-hands-on/src/components/githubApi/GithubUser.tsx
+++ react-hands-on/src/components/githubApi/GithubUser.tsx
@@ -1,4 +1,91 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [login, setLogin] = useState("");
+  return (
+    <>
+      <SearchForm login={login} setLogin={setLogin} />
+      <GitHubUser login={login} />
+    </>
+  );
+};
+
+// hook
+const useFetch = (uri: string) => {
+  const [data, setData] = useState<LoginProps>();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (!uri) return;
+    fetch(uri)
+      .then((data) => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [uri]);
+  return {
+    loading,
+    data,
+    error,
+  };
+};
+
+const GitHubUser = ({ login }: { login: string }) => {
+  const { loading, data, error } = useFetch(
+    `https://api.github.com/users/${login}`
+  );
+  if (error) return <pre>error: {JSON.stringify(error, null, 2)}</pre>;
+  if (loading) return <h1>loading...</h1>;
+  if (!data) return null;
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>Name: {data.name}</p>}
+        {data.location && <p>Location: {data.location}</p>}
+      </div>
+    </div>
+  );
+};
+
+type SearchFormProps = {
+  login: string;
+  setLogin: (login: string) => void;
+};
+
+const SearchForm = ({ login, setLogin }: SearchFormProps) => {
+  const [text, setText] = useState("");
+  const submit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setLogin(text);
+  };
+  return (
+    <>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Github User..."
+        />
+        <button>検索</button>
+      </form>
+    </>
+  );
+};
+
+const Fetch = ({
+  uri,
+  renderSuccess,
+  loadingFallback = <p>loading...</p>,
+  renderError = (error) => <pre>{JSON.stringify(error, null, 2)}</pre>,
+}: FetchProps) => {
+  const { loading, data, error } = useFetch(uri);
+  if (error) return renderError(error);
+  if (loading) return loadingFallback;
+  if (data) return renderSuccess(data);
+};
 
 type LoginProps = {
   name: string;
@@ -7,54 +94,11 @@ type LoginProps = {
   location: string;
 };
 
-const loadJSON = (key: string) =>
-  key && JSON.parse(localStorage.getItem(key) ?? "{}");
-const saveJSON = (key: string, data: LoginProps) =>
-  localStorage.setItem(key, JSON.stringify(data));
-
-/** サンプル(動きません) */
-const GithubUser = ({ login }: { login: string }) => {
-  const [data, setData] = useState<LoginProps>(); // user name
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   console.log(data);
-  //   if (!data) return;
-  //   if (data.login === login) return;
-  //   const { name, avatar_url, location }: LoginProps = data;
-  //   saveJSON(`user:${login}`, {
-  //     name,
-  //     login,
-  //     avatar_url,
-  //     location,
-  //   });
-  // }, [data]);
-  useEffect(() => {
-    if (!login) return;
-    // if (data && data.login === login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (loading) return <h1>loading...</h1>;
-  if (!data) return null;
-
-  return (
-    <div className="githubUser">
-      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
-      <div>
-        <h1>{data.login}</h1>
-        {data.name && <p>{data.name}</p>}
-        {data.location && <p>{data.location}</p>}
-      </div>
-    </div>
-  );
+type FetchProps = {
+  uri: string;
+  renderSuccess: (data: LoginProps) => JSX.Element;
+  loadingFallback: JSX.Element;
+  renderError: (error: Error) => JSX.Element;
 };
 
-export default GithubUser;
+export default App;
