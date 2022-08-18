c581b1b86a6f6a6ab2c11d3df12c0e7431ed97f0
hamadatomoki <hamada.tomoki01@gmail.com>
2022-08-18 18:22:19 +0900

    :books: Learn queries in applo client.

--- apollo-client/src/queries/App.tsx
+++ apollo-client/src/queries/App.tsx
@@ -3,24 +3,45 @@ import {
   ApolloProvider,
   gql,
   InMemoryCache,
+  NetworkStatus,
+  useLazyQuery,
   useQuery,
 } from "@apollo/client";
+import React, { ChangeEvent, useState } from "react";
 
+/** Type */
+type Dog = {
+  id: string;
+  breed: string;
+};
+
+type DogsProps = {
+  onDogSelected?: (e: ChangeEvent<HTMLSelectElement>) => void;
+};
+
+/** Client */
 const client = new ApolloClient({
   uri: "https://71z1g.sse.codesandbox.io/",
   cache: new InMemoryCache(),
 });
 
+/** App */
 const App = () => {
+  const [selectedDog, setSelectedDog] = useState<string | null>(null);
+  const onDogSelected = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectedDog(e.target.value);
   return (
     <ApolloProvider client={client}>
       <div>
-        <Dogs />
+        <Dogs onDogSelected={onDogSelected} />
+        <DogPhoto breed={selectedDog} />
+        <DelayedQuery />
       </div>
     </ApolloProvider>
   );
 };
 
+/** Dogs */
 const GET_DOGS = gql`
   query GetDogs {
     dogs {
@@ -30,12 +51,7 @@ const GET_DOGS = gql`
   }
 `;
 
-type Dog = {
-  id: string;
-  breed: string;
-};
-
-const Dogs = ({ onDogSelected }: { onDogSelected?: () => void }) => {
+const Dogs = ({ onDogSelected }: DogsProps) => {
   const { loading, error, data } = useQuery(GET_DOGS);
 
   if (loading) return <div>"Loading..."</div>;
@@ -49,6 +65,56 @@ const Dogs = ({ onDogSelected }: { onDogSelected?: () => void }) => {
         </option>
       ))}
     </select>
+  );
+};
+
+/** DogPhoto */
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+const DogPhoto = ({ breed }: { breed: string | null }) => {
+  const { loading, error, data, refetch, networkStatus } = useQuery(
+    GET_DOG_PHOTO,
+    {
+      variables: { breed },
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+
+  if (networkStatus === NetworkStatus.refetch) return <div>Refetching!</div>;
+  if (loading) return null;
+  if (error) return <div>{`Error! ${error}`}</div>;
+
+  return (
+    <div>
+      <div>
+        <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+      </div>
+      {/** refetchに{breed: "pug"}のようなオブジェクトを渡すとpugの画像が再フェッチされる */}
+      <button onClick={() => refetch()}>Refresh new breed!</button>
+    </div>
+  );
+};
+
+const DelayedQuery = () => {
+  const [getDog, { loading, error, data }] = useLazyQuery(GET_DOG_PHOTO);
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{`Error! ${error}`}</p>;
+
+  return (
+    <div>
+      {data?.dog && <img src={data.dog.displayImage} />}
+      <button onClick={() => getDog({ variables: { breed: "bulldog" } })}>
+        Click me!
+      </button>
+    </div>
   );
 };
 
